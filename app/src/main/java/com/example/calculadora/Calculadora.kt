b5737c6976fb4e92d2506c5806a2876914ce480b
OfGhetto <matias.elgueta.duarte@gmail.com>
2020-09-20 00:58:39 -0300

    Version 1.0 arreglada

--- app/src/main/java/com/example/calculadora/Calculadora.kt
+++ app/src/main/java/com/example/calculadora/Calculadora.kt
@@ -1,4 +1,27 @@
 package com.example.calculadora
+import java.util.*
+import java.util.regex.Pattern
+import kotlin.collections.ArrayList
 
 class Calculadora {
+    private val pattern = "([()])|(\\d+(\\.\\d+)?)|([+*×÷/-])"
+    private var op:String = ""
+
+    fun getResultado(op:String):Double{
+        this.op = op
+        val tokens = parseTokens(op)
+        return 0.0
+    }
+
+    fun parseTokens(op : String): List<String>{
+        val pattern = Pattern.compile(pattern)
+        val matcher= pattern.matcher(op)
+        val tokens = ArrayList<String>()
+        while (matcher.find()){
+            tokens.add(matcher.group())
+        }
+        return tokens
+    }
+
+
 }
